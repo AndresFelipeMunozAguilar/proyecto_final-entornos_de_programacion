9dfab5f742ec200522e41f305eeb72918f2aee79
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-05-12 16:24:13 -0500

    agregar etiqueta builder en la entidad user

--- backend/book-network/src/main/java/com/entornos/book/user/User.java
+++ backend/book-network/src/main/java/com/entornos/book/user/User.java
@@ -26,17 +26,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name = "user")
+@EntityListeners(AuditingEntityListener.class)
 public class User implements UserDetails, Principal {
 
     @Id
