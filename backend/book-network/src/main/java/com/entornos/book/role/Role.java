2d13b58d3de7523863557fd9852b24c08c64b197
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-05-11 23:22:25 -0500

    update role and user

--- backend/book-network/src/main/java/com/entornos/book/role/Role.java
+++ backend/book-network/src/main/java/com/entornos/book/role/Role.java
@@ -1,16 +1,21 @@
 package com.entornos.book.role;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.entornos.book.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -35,6 +40,10 @@ public class Role {
 
     @Column(name = "name", unique = true)
     private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
 
     @CreatedDate
     @Column(name = "created_date", nullable = false, updatable = false)
--- backend/book-network/src/main/java/com/entornos/book/user/User.java
+++ backend/book-network/src/main/java/com/entornos/book/user/User.java
@@ -7,6 +7,9 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.security.core.userdetails.UserDetails;
+
+import com.entornos.book.role.Role;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -15,8 +18,10 @@ import org.springframework.security.core.GrantedAuthority;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -58,7 +63,8 @@ public class User implements UserDetails, Principal {
     @Column(name = "enabled")
     private boolean enabled;
 
-    // private List<Role> roles;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
 
     @CreatedDate
     @Column(name = "created_date", nullable = false, updatable = false)
