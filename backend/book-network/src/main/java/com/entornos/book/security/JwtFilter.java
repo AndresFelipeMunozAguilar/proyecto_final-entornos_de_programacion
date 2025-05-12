436087272dc40e52a53af3229bb5a9bdd7442965
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-05-12 18:21:01 -0500

    Implementación del filtro JWT

--- backend/book-network/src/main/java/com/entornos/book/security/JwtFilter.java
+++ backend/book-network/src/main/java/com/entornos/book/security/JwtFilter.java
@@ -1,9 +1,49 @@
 package com.entornos.book.security;
 
+import java.io.IOException;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @Service
-public class JwtFilter {
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+        if (request.getServletPath().contains("/api/v1/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String jwt;
+        final String userEmail;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+
+    }
 
     // FALTA IMPLEMENTACIÓN
 
