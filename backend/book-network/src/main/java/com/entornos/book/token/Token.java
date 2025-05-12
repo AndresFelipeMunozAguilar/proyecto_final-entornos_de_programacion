5975085bf75aea0623f85f78353abf2d1c2b83a8
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-05-12 16:23:44 -0500

    crear entidad y repositorio para el token

---
+++ backend/book-network/src/main/java/com/entornos/book/token/Token.java
@@ -0,0 +1,49 @@
+package com.entornos.book.token;
+
+import java.time.LocalDateTime;
+
+import com.entornos.book.user.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "token")
+public class Token {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id_token")
+    private Integer idToken;
+
+    @Column(name = "token")
+    private String token;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "expires_at")
+    private LocalDateTime expiresAt;
+
+    @Column(name = "validated_at")
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
