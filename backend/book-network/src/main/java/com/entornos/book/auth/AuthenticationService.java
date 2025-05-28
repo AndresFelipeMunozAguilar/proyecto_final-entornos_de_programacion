e7418a9cebb5043c843c15833e526248b4ea5229
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-05-28 15:45:40 -0500

    crear scafold/andamio de como debería funcionar el servicio de autenticación (falta implementar muchos métodos)

--- backend/book-network/src/main/java/com/entornos/book/auth/AuthenticationService.java
+++ backend/book-network/src/main/java/com/entornos/book/auth/AuthenticationService.java
@@ -1,12 +1,43 @@
 package com.entornos.book.auth;
 
+import java.util.List;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.entornos.book.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
-@
+@RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
     public void register(RegistrationRequest regRequest) {
+
+        var userRole = roleRepository.findByName("USER")
+                .orElseThrow(() -> new IllegalStateException("User role not initialized"));
+        // Create better exception handling
+
+        var user = User.builder()
+                .firstName(regRequest.getFirstName())
+                .lastName(regRequest.getLastName())
+                .email(regRequest.getEmail())
+                .password(passwordEncoder.encode(regRequest.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+
+        userRepository.save(user);
+
+        sendValidationEmail(user);
+
     }
 
 }
