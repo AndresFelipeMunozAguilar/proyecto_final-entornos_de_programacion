e394033f8f447cb57b05bc33a47a017256b516aa
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-05-28 15:59:10 -0500

    implementar lógica para crear y guardar token de validación

--- backend/book-network/src/main/java/com/entornos/book/auth/AuthenticationService.java
+++ backend/book-network/src/main/java/com/entornos/book/auth/AuthenticationService.java
@@ -1,11 +1,16 @@
 package com.entornos.book.auth;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.entornos.book.role.RoleRepository;
+import com.entornos.book.token.Token;
+import com.entornos.book.token.TokenRepository;
+import com.entornos.book.user.User;
 import com.entornos.book.user.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +22,7 @@ public class AuthenticationService {
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
 
     public void register(RegistrationRequest regRequest) {
 
@@ -38,6 +44,50 @@ public class AuthenticationService {
 
         sendValidationEmail(user);
 
+    }
+
+    private void sendValidationEmail(User user) {
+
+        var newToken = generateAndSaveValidationToken(user);
+
+    }
+
+    private String generateAndSaveValidationToken(User user) {
+
+        String generatedToken = generateActivationCode(6);
+
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateActivationCode(int length) {
+
+        String characters = "0123456789";
+
+        StringBuilder codeBuilder = new StringBuilder();
+
+        SecureRandom secureRandom = new SecureRandom();
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+
+        for (int i = 0; i < length; i++) {
+
+            // Este indice genera valores entre 0 y 9
+            int randomIndex = secureRandom.nextInt(characters.length());
+
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+
+        return codeBuilder.toString();
     }
 
 }
