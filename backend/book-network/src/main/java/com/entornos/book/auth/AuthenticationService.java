0556e6efb5439bf6b453800c0aa4b7b74bc21b67
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-05-28 12:56:01 -0500

    crear servicio de autenticación junto con un método de registro de usuario

---
+++ backend/book-network/src/main/java/com/entornos/book/auth/AuthenticationService.java
@@ -0,0 +1,12 @@
+package com.entornos.book.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@
+public class AuthenticationService {
+
+    public void register(RegistrationRequest regRequest) {
+    }
+
+}
