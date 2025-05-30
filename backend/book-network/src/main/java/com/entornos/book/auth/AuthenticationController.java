8adfe7fcf0a5ef8832f9fb9c3ca10eee273bf96f
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-05-29 23:28:21 -0500

    agregar controlador para autenticar cuenta

--- backend/book-network/src/main/java/com/entornos/book/auth/AuthenticationController.java
+++ backend/book-network/src/main/java/com/entornos/book/auth/AuthenticationController.java
@@ -33,4 +33,12 @@ public class AuthenticationController {
 
     }
 
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody @Valid AuthenticationRequest request) {
+
+        return ResponseEntity.ok(authService.authenticate(request));
+
+    }
+
 }
