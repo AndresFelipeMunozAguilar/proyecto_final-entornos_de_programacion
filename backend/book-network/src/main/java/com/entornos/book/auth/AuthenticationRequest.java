1df2074a6e36012c5365a5ae779c73fc37b711b2
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-05-29 23:28:08 -0500

    crear una clase para indicar como debería ser el cuerpo de la petición de autenticación

---
+++ backend/book-network/src/main/java/com/entornos/book/auth/AuthenticationRequest.java
@@ -0,0 +1,26 @@
+package com.entornos.book.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class AuthenticationRequest {
+
+    @Email(message = "Email format is invalid. Example: username@domain.com")
+    @NotNull(message = "Email is required")
+    @NotBlank(message = "Email must not be blank")
+    private String email;
+
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @NotNull(message = "Password is required")
+    @NotBlank(message = "Password must not be blank")
+    private String password;
+
+}
