89e4ffb8cd1faf1010219fbd118479f33ab0f40f
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-06-01 17:47:27 -0500

    inyectar FormsModule, CommonModule y crear metodo para registrarse y hacer login

--- book-network-ui/src/app/pages/register/register.component.ts
+++ book-network-ui/src/app/pages/register/register.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RegistrationRequest } from '../../services/models';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  registerRequest: RegistrationRequest = { firstName: '', lastName: '', email: '', password: '' }
+  errorMsg: Array<string> = [];
+
+  constructor(
+    private router: Router
+  ) { }
+
+  register() {
+    throw new Error('Method not implemented.');
+  }
+
+  login() {
+    throw new Error('Method not implemented.');
+  }
 
 }
