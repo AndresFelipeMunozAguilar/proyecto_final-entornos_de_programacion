215ad1069ddd96dfc7db6a00983dcdd0aed72d68
Sergio Baron <sbaronrivera@gmail.com>
2025-06-01 00:55:47 -0500

    Excepciones

--- backend/book-network/src/main/java/com/entornos/book/user/User.java
+++ backend/book-network/src/main/java/com/entornos/book/user/User.java
@@ -7,6 +7,9 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.entornos.book.book.Book;
+import com.entornos.book.history.BookTransactionHistory;
+import jakarta.persistence.*;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.entornos.book.role.Role;
@@ -17,16 +20,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -73,6 +66,12 @@ public class User implements UserDetails, Principal {
     @JoinTable(name = "users_roles", // Nombre personalizado de la tabla intermedia
             joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private List<Role> roles;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
+
+    @OneToMany(mappedBy = "user")
+    private List<BookTransactionHistory> histories;
 
     @CreatedDate
     @Column(name = "created_date", nullable = false, updatable = false)
