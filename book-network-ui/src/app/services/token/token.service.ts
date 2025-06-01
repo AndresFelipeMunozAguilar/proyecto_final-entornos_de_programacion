3f84214327be22211b729ccb2adf5a82890c6a29
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-06-01 17:17:27 -0500

    implementar getter y setter para el token de sesión

--- book-network-ui/src/app/services/token/token.service.ts
+++ book-network-ui/src/app/services/token/token.service.ts
@@ -5,5 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class TokenService {
 
-  constructor() { }
+
+  public set token(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+
+  public get token(): string {
+    return localStorage.getItem('token') as string;
+  }
+
+
 }
