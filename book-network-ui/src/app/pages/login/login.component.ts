79958649aad9e8815f3623ab1584e00ccbd9c2bc
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-05-31 22:36:22 -0500

    Implementar varaibles, imports y métodos para el componente de login

--- book-network-ui/src/app/pages/login/login.component.ts
+++ book-network-ui/src/app/pages/login/login.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { AuthenticationRequest } from '../../services/models';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+
+
+  authRequest: AuthenticationRequest = { email: '', password: '' };
+  errorMsg: Array<string> = [];
+
+  login() {
+    throw new Error('Method not implemented.');
+  }
+
+  register() {
+    throw new Error('Method not implemented.');
+  }
 
 }
