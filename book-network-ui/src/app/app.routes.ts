eafa9aa49f10e7c2a1b6152dfabb6f7684fc0b4f
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-06-03 16:14:13 -0500

    TODO: Implementar las rutas de libros y activate-account en la aplicación

--- book-network-ui/src/app/app.routes.ts
+++ book-network-ui/src/app/app.routes.ts
@@ -1,8 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { authGuard } from './services/guard/auth.guard';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'books',
+        pathMatch: 'full'
+    },
     {
         path: 'login',
         component: LoginComponent
@@ -12,4 +19,14 @@ export const routes: Routes = [
         path: 'register',
         component: RegisterComponent
     },
+    // TODO: Crear los componentes de books y activate-account
+    // {
+    //     path: 'activate-account',
+    //     component: ActivateAccountComponent
+    // },
+    // {
+    //     path: 'books',
+    //     loadChildren: () => import('./modules/book/book.module').then(m => m.BookModule),
+    //     canActivate: [authGuard]
+    // }
 ];
