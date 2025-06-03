27bb9bd02ea3e9051386ec9765935398fc663aa0
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-06-03 17:24:35 -0500

    añadir ruta del componente de activar cuenta

--- book-network-ui/src/app/app.routes.ts
+++ book-network-ui/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { authGuard } from './services/guard/auth.guard';
 import { NgModule } from '@angular/core';
+import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 
 export const routes: Routes = [
     {
@@ -19,11 +20,11 @@ export const routes: Routes = [
         path: 'register',
         component: RegisterComponent
     },
+    {
+        path: 'activate-account',
+        component: ActivateAccountComponent
+    },
     // TODO: Crear los componentes de books y activate-account
-    // {
-    //     path: 'activate-account',
-    //     component: ActivateAccountComponent
-    // },
     // {
     //     path: 'books',
     //     loadChildren: () => import('./modules/book/book.module').then(m => m.BookModule),
