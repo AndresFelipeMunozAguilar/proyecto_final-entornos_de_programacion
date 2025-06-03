499fd1887b391f7f2334521e0b615df34ea513a4
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-06-03 15:00:12 -0500

    implemntar funciones para roles de usuario y validez del token

--- book-network-ui/src/app/services/token/token.service.ts
+++ book-network-ui/src/app/services/token/token.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -17,37 +18,52 @@ export class TokenService {
 
 
   // TODO : IMPLEMENTAR EL JWTHELPETSERVICE PARA QUE FUNCIONE
-  // isTokenValid() {
-  //   const token = this.token;
-  //   if (!token) {
-  //     return false;
-  //   }
-  //   // decode the token
-  //   const jwtHelper = new JwtHelperService();
-  //   // check expiry date
-  //   const isTokenExpired = jwtHelper.isTokenExpired(token);
-  //   if (isTokenExpired) {
-  //     localStorage.clear();
-  //     return false;
-  //   }
-  //   return true;
-  // }
+  isTokenValid() {
+
+    const token = this.token;
+    if (!token) {
+
+      return false;
+
+    }
+
+    // decode the token
+    const jwtHelper = new JwtHelperService();
+    // check expiry date
+    const isTokenExpired = jwtHelper.isTokenExpired(token);
+
+    if (isTokenExpired) {
+
+      localStorage.clear();
+      return false;
+
+    }
+    return true;
+
+  }
 
   // TODO : IMPLEMENTAR EL JWTHELPETSERVICE PARA QUE FUNCIONE
-  // isTokenNotValid() {
-  //   return !this.isTokenValid();
-  // }
+  isTokenNotValid() {
+
+    return !this.isTokenValid();
+
+  }
 
   // TODO : IMPLEMENTAR EL JWTHELPETSERVICE PARA QUE FUNCIONE
-  // get userRoles(): string[] {
-  //   const token = this.token;
-  //   if (token) {
-  //     const jwtHelper = new JwtHelperService();
-  //     const decodedToken = jwtHelper.decodeToken(token);
-  //     console.log(decodedToken.authorities);
-  //     return decodedToken.authorities;
-  //   }
-  //   return [];
-  // }
+  get userRoles(): string[] {
+
+    const token = this.token;
+
+    if (token) {
+
+      const jwtHelper = new JwtHelperService();
+      const decodedToken = jwtHelper.decodeToken(token);
+      console.log(decodedToken.authorities);
+      return decodedToken.authorities;
+
+    }
+    return [];
+
+  }
 
 }
