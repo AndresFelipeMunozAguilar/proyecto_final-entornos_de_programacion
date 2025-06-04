e429fdaf5416c71d2c0a65dfc80cb7e1123de574
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-06-03 22:29:06 -0500

    fix main.component.ts

--- book-network-ui/src/app/modules/book/pages/main/main.component.ts
+++ book-network-ui/src/app/modules/book/pages/main/main.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../../components/menu/menu.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
+  imports: [MenuComponent, RouterOutlet],
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
