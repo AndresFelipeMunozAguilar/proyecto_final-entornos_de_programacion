c47f81872b447e4ce9aa1c0fb3db53327de33657
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-06-04 01:21:21 -0500

    importar bien las rutas de libros

--- book-network-ui/src/app/app.routes.ts
+++ book-network-ui/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { authGuard } from './services/guard/auth.guard';
-import { NgModule } from '@angular/core';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 
 export const routes: Routes = [
@@ -15,7 +14,6 @@ export const routes: Routes = [
         path: 'login',
         component: LoginComponent
     },
-
     {
         path: 'register',
         component: RegisterComponent
@@ -26,7 +24,7 @@ export const routes: Routes = [
     },
     {
         path: 'books',
-        loadChildren: () => import('./modules/book/book.module').then(m => m.BookModule),
+        loadChildren: () => import('./modules/book/book-routing.module').then(m => m.bookRoutes),
         canActivate: [authGuard]
     }
 ];
