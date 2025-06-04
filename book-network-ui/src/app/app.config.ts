c7fa38cca5cb762e4de8a6c740e1bf4d2bf0e036
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-06-04 01:21:04 -0500

    arregla HttpTokenInterceptor

--- book-network-ui/src/app/app.config.ts
+++ book-network-ui/src/app/app.config.ts
@@ -1,15 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { httpTokenInterceptor } from './services/interceptor/http-token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch()) // Se agrega el proveedor de HttpClient aquí
+    provideHttpClient(
+      withInterceptors([httpTokenInterceptor])
+    ), // Se agrega el proveedor de HttpClient aquí
   ]
 };
