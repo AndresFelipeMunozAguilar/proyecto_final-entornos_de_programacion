b7baff4068412775353ace8e7f2da82845d709d8
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-06-04 01:22:04 -0500

    arreglar el comportamiento de menu.component

--- book-network-ui/src/app/modules/book/components/menu/menu.component.ts
+++ book-network-ui/src/app/modules/book/components/menu/menu.component.ts
@@ -1,26 +1,35 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
+  imports: [RouterLink, RouterLinkActive],
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
   ngOnInit(): void {
     const linkColor = document.querySelectorAll('.nav-link');
     linkColor.forEach(link => {
-      if (window.location.href.endsWith(link.getAttribute('href') || '')) {
+
+      if (window.location.href.endsWith(link.getAttribute('routerLink') || '')) {
+
         link.classList.add('active');
+
       }
       link.addEventListener('click', () => {
+
         linkColor.forEach(l => l.classList.remove('active'));
         link.classList.add('active');
+
       });
     });
   }
 
   logout() {
+
     localStorage.removeItem('token');
     window.location.reload();
+
   }
 }
